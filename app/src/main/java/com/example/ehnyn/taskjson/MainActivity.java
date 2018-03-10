264cd2369936d9ede48b8f6b8bf73e6dfb1c8567
oluwabamise <eniolaipoola@gmail.com>
2018-03-10 10:52:11 +0000

    Project Structure

---
+++ app/src/main/java/com/example/ehnyn/taskjson/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.ehnyn.taskjson;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button postButton = (Button) findViewById(R.id.btnPost);
+        Button commentButton = (Button) findViewById(R.id.btnComment);
+
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent postIntent = new Intent(MainActivity.this, PostActivity.class);
+                startActivity(postIntent);
+            }
+        });
+
+        commentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent commentIntent = new Intent(MainActivity.this, CommentActivity.class);
+                startActivity(commentIntent);
+            }
+        });
+
+    }
+}
